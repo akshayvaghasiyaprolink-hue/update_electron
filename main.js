0b4c73e6befd06fc72849c03bfbfbe34ce312aad
-g <-g>
2026-02-12 11:16:08 +0530

    added changes

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("path");
+const { autoUpdater } = require("electron-updater");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  autoUpdater.checkForUpdatesAndNotify();
+
+  autoUpdater.on("update-available", () => {
+    mainWindow.webContents.send("update_available");
+  });
+
+  autoUpdater.on("update-downloaded", () => {
+    mainWindow.webContents.send("update_downloaded");
+  });
+});
+
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ renderer.js
@@ -0,0 +1,15 @@
+const status = document.getElementById("status");
+const restartBtn = document.getElementById("restartBtn");
+
+window.electronAPI.onUpdateAvailable(() => {
+  status.innerText = "⬇️ Update Available... Downloading...";
+});
+
+window.electronAPI.onUpdateDownloaded(() => {
+  status.innerText = "✅ Update Downloaded. Restart to Install!";
+  restartBtn.style.display = "block";
+});
+
+restartBtn.addEventListener("click", () => {
+  window.electronAPI.restartApp();
+});
