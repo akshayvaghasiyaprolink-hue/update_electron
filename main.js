3f6a9cd0a9c5dfebceee4016b4bb47cc54e114b5
-g <-g>
2026-02-12 14:39:38 +0530

    add

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
-
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
+
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
 
 log.transports.file.level = "info";
 autoUpdater.logger = log;
@@ -26,49 +29,36 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow();
 
-  log.info("✅ App started...");
-  log.info("App Version:", app.getVersion());
-
-  autoUpdater.autoDownload = true;
-
-  setTimeout(() => {
-    log.info("🔍 Checking for updates...");
-    autoUpdater.checkForUpdatesAndNotify();
-  }, 2000);
+  autoUpdater.checkForUpdatesAndNotify();
 
   autoUpdater.on("checking-for-update", () => {
-    log.info("🔍 Checking for update...");
-    mainWindow.webContents.send("checking_for_update");
+    log.info("Checking for update...");
   });
 
-  autoUpdater.on("update-available", (info) => {
-    log.info("✅ Update available:", info);
-    mainWindow.webContents.send("update_available", info);
+  autoUpdater.on("update-available", () => {
+    log.info("Update available");
+    mainWindow.webContents.send("update_available");
   });
 
-  autoUpdater.on("update-not-available", (info) => {
-    log.info("❌ No update available:", info);
-    mainWindow.webContents.send("update_not_available", info);
-  });
-
-  autoUpdater.on("download-progress", (progress) => {
-    log.info("⬇ Download progress:", progress.percent);
-    mainWindow.webContents.send("download_progress", progress);
-  });
-
-  autoUpdater.on("update-downloaded", (info) => {
-    log.info("✅ Update downloaded:", info);
-    mainWindow.webContents.send("update_downloaded", info);
+  autoUpdater.on("update-not-available", () => {
+    log.info("No update available");
   });
 
   autoUpdater.on("error", (err) => {
-    log.error("🚨 Update error:", err);
-    mainWindow.webContents.send("update_error", err.toString());
+    log.error("Updater error:", err);
+  });
+
+  autoUpdater.on("download-progress", (progress) => {
+    log.info("Download progress:", progress);
+  });
+
+  autoUpdater.on("update-downloaded", () => {
+    log.info("Update downloaded");
+    mainWindow.webContents.send("update_downloaded");
   });
 });
 
 ipcMain.on("restart_app", () => {
-  log.info("🔁 Restarting app to install update...");
   autoUpdater.quitAndInstall();
 });
 
