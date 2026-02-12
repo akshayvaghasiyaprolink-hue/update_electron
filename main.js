1a5eb4290ee4e9e35b1c20681a4d205727a8b5ae
-g <-g>
2026-02-12 15:19:18 +0530

    add

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
-const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
-
-if (require("electron-squirrel-startup")) {
-  app.quit();
-}
-
-log.transports.file.level = "info";
-autoUpdater.logger = log;
+const { autoUpdater } = require("electron-updater");
 
 let mainWindow;
+
+// ✅ Logging setup
+log.transports.file.level = "info";
+autoUpdater.logger = log;
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = true;
+
+// ✅ Fix for Squirrel Startup Events (without installing electron-squirrel-startup)
+if (process.platform === "win32") {
+  const squirrelEvent = process.argv[1];
+
+  if (squirrelEvent && squirrelEvent.startsWith("--squirrel")) {
+    app.quit();
+  }
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -24,44 +32,68 @@ function createWindow() {
   });
 
   mainWindow.loadFile("index.html");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  autoUpdater.checkForUpdatesAndNotify();
+  log.info("App Started...");
+  log.info("App Version: " + app.getVersion());
 
+  // ✅ Check update after window ready
+  setTimeout(() => {
+    log.info("Checking for updates...");
+    autoUpdater.checkForUpdatesAndNotify();
+  }, 2000);
+
+  // ✅ Update Events
   autoUpdater.on("checking-for-update", () => {
     log.info("Checking for update...");
+    mainWindow?.webContents.send("update_status", "Checking for update...");
   });
 
-  autoUpdater.on("update-available", () => {
-    log.info("Update available");
-    mainWindow.webContents.send("update_available");
+  autoUpdater.on("update-available", (info) => {
+    log.info("Update available: " + JSON.stringify(info));
+    mainWindow?.webContents.send("update_available");
+    mainWindow?.webContents.send("update_status", "Update available... Downloading...");
   });
 
-  autoUpdater.on("update-not-available", () => {
-    log.info("No update available");
+  autoUpdater.on("update-not-available", (info) => {
+    log.info("Update not available: " + JSON.stringify(info));
+    mainWindow?.webContents.send("update_status", "You already have the latest version.");
   });
 
   autoUpdater.on("error", (err) => {
-    log.error("Updater error:", err);
+    log.error("Update error: " + err.toString());
+    mainWindow?.webContents.send("update_status", "Update error: " + err.message);
   });
 
-  autoUpdater.on("download-progress", (progress) => {
-    log.info("Download progress:", progress);
+  autoUpdater.on("download-progress", (progressObj) => {
+    let msg = `Downloading: ${Math.round(progressObj.percent)}%`;
+    log.info(msg);
+    mainWindow?.webContents.send("update_status", msg);
   });
 
-  autoUpdater.on("update-downloaded", () => {
-    log.info("Update downloaded");
-    mainWindow.webContents.send("update_downloaded");
+  autoUpdater.on("update-downloaded", (info) => {
+    log.info("Update downloaded: " + JSON.stringify(info));
+    mainWindow?.webContents.send("update_downloaded");
+    mainWindow?.webContents.send("update_status", "Update downloaded. Restart to install.");
   });
 });
 
+// ✅ Restart & Install Update
 ipcMain.on("restart_app", () => {
+  log.info("Restart button clicked. Installing update...");
   autoUpdater.quitAndInstall();
 });
 
+// ✅ Quit app when all windows closed
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
--- preload.js
+++ preload.js
@@ -1,25 +1,10 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  // update events
-  onCheckingForUpdate: (callback) =>
-    ipcRenderer.on("checking_for_update", callback),
+  onUpdateAvailable: (callback) => ipcRenderer.on("update_available", callback),
+  onUpdateDownloaded: (callback) => ipcRenderer.on("update_downloaded", callback),
 
-  onUpdateAvailable: (callback) =>
-    ipcRenderer.on("update_available", callback),
+  onUpdateStatus: (callback) => ipcRenderer.on("update_status", callback),
 
-  onUpdateNotAvailable: (callback) =>
-    ipcRenderer.on("update_not_available", callback),
-
-  onDownloadProgress: (callback) =>
-    ipcRenderer.on("download_progress", callback),
-
-  onUpdateDownloaded: (callback) =>
-    ipcRenderer.on("update_downloaded", callback),
-
-  onUpdateError: (callback) =>
-    ipcRenderer.on("update_error", callback),
-
-  // restart
   restartApp: () => ipcRenderer.send("restart_app"),
 });
